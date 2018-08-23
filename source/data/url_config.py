6a3e2754649042ac059038fd0c9935164a7df61d
Sanjay Singh Panwar <snjsinghimp@gmail.com>
2018-08-23 17:43:28 +0530

    optimized code

---
+++ source/data/url_config.py
@@ -0,0 +1,2 @@
+active_url = "https://www.monkeytest1.com/"
+# active_url = "https://www.surveymonkey.com/"
---
+++ source/data/user_conf.py
@@ -0,0 +1,4 @@
+valid_username = "qa.user"
+valid_password = "Pass_123"
+invalid_username = "sample"
+invalid_password = "12345"
